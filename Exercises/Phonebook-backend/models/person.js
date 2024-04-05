8e3c5ca7e888e59c4e974633f4f3b219a388939a
Agnieszka Nowacka <agnieszka.nowacka@object.pl>
2024-04-05 15:44:10 +0200

    custom validation

--- Exercises/Phonebook-backend/models/person.js
+++ Exercises/Phonebook-backend/models/person.js
@@ -21,7 +21,12 @@ const personSchema = new mongoose.Schema({
     number: 
     {
         type: String,
-        minLength: 9,
+        validate: {
+            validator: function(v) {
+              return /\d{2}\s\d{9}/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!`
+          },
         required: true
     },
 })
